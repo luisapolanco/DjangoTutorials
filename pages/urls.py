0febf1a7667d78095d73b91d49be554b8000b0eb
luisapolanco <lmpolanco1@eafit.edu.co>
2024-02-15 22:21:13 -0500

    Project created

---
+++ pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+
+from .views import AboutPageView, ContactView, HomePageView, ProductCreateView, ProductIndexView, ProductShowView  
+
+urlpatterns = [ 
+    path("", HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'), 
+    path('products/', ProductIndexView.as_view(), name='index'), 
+    path('products/create', ProductCreateView.as_view(), name='form'), 
+    path('products/<str:id>', ProductShowView.as_view(), name='show'), 
+] 
